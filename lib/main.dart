4932e35eccf9d7f2923214db466c3bced857730d
rriakang <kangrira123@gmail.com>
2024-03-13 01:39:15 +0900

    bottombar

--- lib/main.dart
+++ lib/main.dart
@@ -1,226 +1,117 @@
-import 'package:flutter/material.dart'; //플러터 머터리얼 import 라이브러리
-
-
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-
-
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'First app',
-      theme: ThemeData.light().copyWith( //디자인
-        appBarTheme: AppBarTheme(
+      theme: ThemeData.light().copyWith(
+        appBarTheme: AppBarTheme(),
       ),
-      ),
-      home: MyCard(),//앱이 정상적으로 실행되었을때 가장 먼저 보여주는 경로
+      home: MyCard(),
     );
   }
-
-
 }
 
 class MyCard extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    //scaffold : 중요함 , 어떠한 요소들도 . 앱화면에 배치될수없음
+  Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         backgroundColor: Color.fromARGB(1, 250, 250, 250),
-        // 데이터 받을곳 (근무지)
-        title: Text('Seoul Hospital'), //앱 화면에 출력되는 타이틀 --> 데이터 받을곳
+        title: Text('Seoul Hospital'),
         centerTitle: true,
         elevation: 0.0,
-
       ),
       drawer: Drawer(
         child: ListView(
-          padding: EdgeInsets.zero, //여백 x
+          padding: EdgeInsets.zero,
           children: [
             UserAccountsDrawerHeader(
               accountName: Text("강리라"),
               accountEmail: Text("rirakang@gachon.ac.kr"),
-              currentAccountPicture: CircleAvatar(
-
-              ),
-
-              onDetailsPressed: () => {
-                print("clicked")
-              },
+              currentAccountPicture: CircleAvatar(),
+              onDetailsPressed: () => print("clicked"),
               decoration: BoxDecoration(
                 color: Colors.red[200],
-
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home, color:Colors.grey[850],), // 첫 부분
+              leading: Icon(Icons.home, color:Colors.grey[850]),
               title: Text("Duty 유형 수정"),
-              onTap: () => {
-                print("home!!")
-              },
-
+              onTap: () => print("home!!"),
             ),
             ListTile(
-              leading: Icon(Icons.group, color:Colors.grey[850],), // 첫 부분
+              leading: Icon(Icons.group, color:Colors.grey[850]),
               title: Text("공유하기"),
-              onTap: () => {
-                print("공유하기")
-              },
-
+              onTap: () => print("공유하기"),
             ),
             ListTile(
-              leading: Icon(Icons.settings, color:Colors.grey[850],), // 첫 부분
+              leading: Icon(Icons.settings, color:Colors.grey[850]),
               title: Text("설정"),
-              onTap: () => {
-                print("Setting!!")
-              },
-
+              onTap: () => print("Setting!!"),
             ),
             ListTile(
-              leading: Icon(Icons.question_answer, color:Colors.grey[850],), // 첫 부분
+              leading: Icon(Icons.question_answer, color:Colors.grey[850]),
               title: Text("Q&A"),
-              onTap: () => {
-                print("Q&A!!")
-              },
-
+              onTap: () => print("Q&A!!"),
             ),
           ],
-
-
         ),
       ),
-
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, //시작점 정렬
-          children: <Widget>[
-            Center(
-              child : CircleAvatar(
-              radius: 60.0,
-             ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0, //divder 선 margin같은 느낌
-            ),
-            Text('NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0, //간격 조절
-            ),
-            Text('test',
-              style: TextStyle(
-                color:  Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-              ),
-
-            ), SizedBox(
-              height: 30.0, //간격 조절
-            ),
-            Text('test',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0, //간격 조절
-            ),
-            Text('100',
-              style: TextStyle(
-                  color:  Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-              ),
-
-            ),SizedBox(
-              height: 30.0, //간격 조절
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width : 10.0,
-                ),
-                Text('test',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0
-                ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width : 10.0,
-                ),
-                Text('test',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width : 10.0,
-                ),
-                Text('test',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-                  ),
-                ),
-              ],
-
-            ),Row(
-              children: <Widget>[
-
-                SizedBox(
-                  width: 10.0,
-                  height : 200.0,
-                ),
-                Text('test',
-                  style: TextStyle(
-                      fontSize: 16.0,
-                      letterSpacing: 1.0
-
-                  ),
-                ),
-
-              ],
-            ),
-
-
-
-
-
-          ],
-        )
-      ),
-
-
+      // bottomNavigationBar: DefaultTabController(
+      //   length: 3,
+      //   child: Builder(
+      //     builder: (BuildContext context) {
+      //       return SizedBox(
+      //         height: 70,
+      //         child: Column(
+      //           children: [
+      //             TabBar(
+      //               tabs: <Widget>[
+      //                 Tab(
+      //                   icon: Icon(
+      //                     Icons.person,
+      //                     color: Colors.black,
+      //                   ),
+      //                 ),
+      //                 Tab(
+      //                   icon: Icon(
+      //                     Icons.home,
+      //                     color: Colors.black,
+      //                   ),
+      //                 ),
+      //                 Tab(
+      //                   icon: Icon(
+      //                     Icons.chat_bubble,
+      //                     color: Colors.black,
+      //                   ),
+      //                 ),
+      //               ],
+      //             ),
+      //             Expanded(
+      //               child: TabBarView(
+      //                 children: <Widget>[
+      //                   Container(
+      //                     child: Text('Person Tab Content'),
+      //                   ),
+      //                   Container(
+      //                     child: Text('Home Tab Content'),
+      //                   ),
+      //                   Container(
+      //                     child: Text('Chat Tab Content'),
+      //                   ),
+      //                 ],
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       );
+      //     },
+      //   ),
+      // ),
     );
   }
-
 }
