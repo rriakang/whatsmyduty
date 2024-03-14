3714f68f77ff70cc7967b432e2fde45300a06cb8
rriakang <kangrira123@gmail.com>
2024-03-14 23:58:57 +0900

    login_button

--- lib/screens/start_page.dart
+++ lib/screens/start_page.dart
@@ -5,67 +5,74 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 250.0),
-            child: Align(
-              alignment: Alignment.center,
-              child: Text(
-                'whats my duty?',
-                style: TextStyle(fontSize: 24.0), // 글꼴 크기를 24로 설정
+      body: SingleChildScrollView( // Ensure the content fits on small devices
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 250.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'whats my duty?',
+                  style: TextStyle(fontSize: 24.0), // 글꼴 크기를 24로 설정
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(5.0),
-            child: Align(
-              alignment: Alignment.center,
-              child: Text('간편 회원가입으로 시작하세요'),
-            ),
-          ),
-          // 네이버로 시작하기 버튼
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // 네이버 로그인 로직
-              },
-              child: Text('네이버로 시작하기'),
-              style: ButtonStyle(
-
-                backgroundColor: MaterialStateProperty.all(Colors.green),
+            Container(
+              margin: EdgeInsets.all(5.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text('간편 회원가입으로 시작하세요'),
               ),
             ),
-          ),
-          // 카카오로 시작하기 버튼
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // 카카오 로그인 로직
-              },
-              child: Text('카카오로 시작하기'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.yellow),
+            // 네이버로 시작하기 버튼
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+              child: ElevatedButton.icon(
+                icon: Image.asset('assets/naver_icon.png', height: 18.0), // 네이버 아이콘
+                label: Text(
+                  '네이버로 시작하기',
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
+                ),
+                onPressed: () {
+                  // 네이버 로그인 로직
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF1EC800)), // 네이버 색상
+                  padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 12.0)),
+                  textStyle: MaterialStateProperty.all(TextStyle(fontWeight: FontWeight.bold)),
+                ),
               ),
             ),
-          ),
-          // 애플로 시작하기 버튼
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // 애플 로그인 로직
-              },
-              child: Text('애플로 시작하기'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.black),
+            // 카카오로 시작하기 버튼
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // 카카오 로그인 로직
+                },
+                child: Text('카카오로 시작하기'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                ),
               ),
             ),
-          ),
-        ],
+            // 애플로 시작하기 버튼
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // 애플 로그인 로직
+                },
+                child: Text('애플로 시작하기'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
