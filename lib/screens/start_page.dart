d1ef6d45395c9d6536c03443ccda9be4efadf97e
rriakang <kangrira123@gmail.com>
2024-03-14 23:54:15 +0900

    login_button

--- lib/screens/start_page.dart
+++ lib/screens/start_page.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-//로그인 회원가입 화면 뭘로 할지는 참고해야됌
 class StartPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Text("로그인 하시겠습니까?"),
-            ));
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 250.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'whats my duty?',
+                style: TextStyle(fontSize: 24.0), // 글꼴 크기를 24로 설정
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(5.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text('간편 회원가입으로 시작하세요'),
+            ),
+          ),
+          // 네이버로 시작하기 버튼
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // 네이버 로그인 로직
+              },
+              child: Text('네이버로 시작하기'),
+              style: ButtonStyle(
+
+                backgroundColor: MaterialStateProperty.all(Colors.green),
+              ),
+            ),
+          ),
+          // 카카오로 시작하기 버튼
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // 카카오 로그인 로직
+              },
+              child: Text('카카오로 시작하기'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.yellow),
+              ),
+            ),
+          ),
+          // 애플로 시작하기 버튼
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 50.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // 애플 로그인 로직
+              },
+              child: Text('애플로 시작하기'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
